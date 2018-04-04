5a89bd9be2f3c1ca18f54337e923620896d5727d
DESKTOP-2KNFIFD\IgorMare <igor.marenjak@tarmel.hr>
2018-04-04 15:47:27 +0200

    moj prvi Algebra commit

---
+++ VektorPovecanjeKapaciteta/PrazanVektor.cpp
@@ -0,0 +1,16 @@
+#include "stdafx.h"
+#include "PrazanVektor.h"
+
+
+PrazanVektor::PrazanVektor()
+{
+}
+
+
+PrazanVektor::PrazanVektor(std::initializer_list<int> il)
+{
+}
+
+PrazanVektor::~PrazanVektor()
+{
+}
---
+++ VektorPovecanjeKapaciteta/PrazanVektor.h
@@ -0,0 +1,9 @@
+#pragma once
+#include<initializer_list>
+class PrazanVektor
+{
+public:
+	PrazanVektor(std::initializer_list<int> il);
+	~PrazanVektor();
+};
+
